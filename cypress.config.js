039fd2ba904d0b435bae088088eadf392f61b8af
vireshnalge <110518256+vireshnalge@users.noreply.github.com>
2023-02-14 21:10:24 +0530

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,19 +1,13 @@
 const { defineConfig } = require("cypress");
-const { lighthouse, prepareAudit } = require("@cypress-audit/lighthouse");
 
 module.exports = defineConfig({
+  projectId: '7g21dx',
+  videoCompression: 15,
   e2e: {
-    baseUrl: "https://www.lambdatest.com/selenium-playground/input-form-demo",
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      on("before:browser:launch", (browser = {}, launchOptions) => {
-        prepareAudit(launchOptions);
-      });
-
-      on("task", {
-        lighthouse: lighthouse(),
-        
-      });
+      
     },
   },
 });
+
